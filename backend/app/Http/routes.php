3bfc2d6de04f183966f33abe4fcac2c7dece22a3
eran.finkle@worthy.com <eran.finkle@worthy.com>
2019-11-23 23:22:32 +0200

    Added Authentication methods

--- backend/app/Http/routes.php
+++ backend/app/Http/routes.php
@@ -11,6 +11,36 @@
 |
 */
 
+// API Routes come first
+$api = app('Dingo\Api\Routing\Router');
+$api->version('v1',function($api){
+    header('Access-Control-Allow-Origin: http://localhost:4200');
+    header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization');
+    header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, PATCH, DELETE');
+
+    // API
+    $api->group(['namespace'=>'App\Http\Controllers\Api'],function($api){
+        // Auth
+        $api->post('auth/login','Auth\AuthController@postLogin');
+        $api->post('auth/token-refresh','Auth\AuthController@refreshToken');
+        $api->post('users','Auth\UsersController@store');
+
+        // Protected methods (require auth)
+        $api->group(['middleware'=>'api.auth'],function($api){
+            
+        });
+
+        // Public methods
+        
+    });
+});
+
+// Catchall - Displays Ember app
+//Route::any('{catchall}',function(){
+//    return view('index');
+//})->where('catchall', '(.*)');
+
+
 Route::get('/', function () {
     return view('welcome');
 });
